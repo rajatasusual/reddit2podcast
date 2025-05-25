6bffe0d8b47b2bb4c12b84f670f3ede1316116b1
Rajatasusual <krajat4@gmail.com>
2025-05-25 22:09:32 +0100

    Adding nuance to the ssml synthesizer

--- src/functions/main.js
+++ src/functions/main.js
@@ -60,39 +60,96 @@ async function generateSsmlEpisode(threads, context) {
 
   context.log(`Generating SSML for ${threads.length} threads`);
 
-  let ssml = `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en-US">`;
+  let ssml = `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis"
+    xmlns:mstts="http://www.w3.org/2001/mstts"
+    xml:lang="en-US">`;
 
-  // Episode Intro
-  ssml += `<voice name="${hostVoice}"><p>Welcome to today's episode of Reddit Top Threads. Let's dive in!<break time="1s"/></p></voice>`;
-
-  //convert threads into documents with titles and comments for summarizer
-  const documents = threads.map(thread => `${thread.title} ${thread.comments.join(' ')}`);
+  // Episode Intro with expressive style
+  ssml += `
+    <voice name="${hostVoice}">
+      <mstts:express-as style="cheerful">
+        <prosody rate="medium">
+          <s>Welcome to today's episode of Reddit Top Threads.</s>
+          <s>Let's dive in!<break time="800ms"/></s>
+        </prosody>
+      </mstts:express-as>
+    </voice>
+  `;
 
   // Summarize the documents
+  const documents = threads.map(thread => `${thread.title} ${thread.comments.join(' ')}`);
   context.log("Summarizing documents...");
   const summary = await extractiveSummarization(documents, context);
   context.log("Summarization complete.");
 
-  // Add the summary to the SSML
-  ssml += `<voice name="${hostVoice}"><p>${escapeXml(summary)}</p></voice>`;
+  ssml += `
+    <voice name="${hostVoice}">
+      <mstts:express-as style="narration-professional">
+        <prosody rate="medium">
+          <s>${escapeXml(summary)}</s>
+        </prosody>
+      </mstts:express-as>
+    </voice>
+  `;
 
+  // Main body - iterate threads
   threads.forEach((thread, idx) => {
     context.log(`Synthesizing SSML for thread: ${thread.title}`);
 
-    ssml += `<voice name="${hostVoice}"><p>Thread ${idx + 1}: ${escapeXml(thread.title)}<break time="300ms"/></p></voice>`;
+    // Host introduces the thread
+    ssml += `
+      <voice name="${hostVoice}">
+        <mstts:express-as style="newscast-casual">
+          <prosody rate="medium">
+            <s>Thread ${idx + 1}: ${escapeXml(thread.title)}<break time="400ms"/></s>
+          </prosody>
+        </mstts:express-as>
+      </voice>
+    `;
+
+    // Comments
     thread.comments.forEach((comment, i) => {
-      ssml += `<voice name="${commenterVoice}"><p>Commenter ${i + 1} says: ${escapeXml(comment)}<break time="500ms"/></p></voice>`;
+      ssml += `
+        <voice name="${commenterVoice}">
+          <mstts:express-as style="friendly">
+            <prosody rate="medium">
+              <s>Commenter ${i + 1} says: ${escapeXml(comment)}</s>
+              <break time="600ms"/>
+            </prosody>
+          </mstts:express-as>
+        </voice>
+      `;
     });
 
+    // Transition to next thread
     if (idx < threads.length - 1) {
-      ssml += `<voice name="${hostVoice}"><p>And now, moving on to the next hot topic.<break time="1s"/></p></voice>`;
+      ssml += `
+        <voice name="${hostVoice}">
+          <mstts:express-as style="cheerful">
+            <prosody rate="medium">
+              <s>And now, moving on to the next hot topic.</s>
+              <break time="1s"/>
+            </prosody>
+          </mstts:express-as>
+        </voice>
+      `;
     }
   });
 
   // Outro
-  ssml += `<voice name="${hostVoice}"><p>That wraps up our episode. Thanks for listening!<break time="1s"/></p></voice>`;
+  ssml += `
+    <voice name="${hostVoice}">
+      <mstts:express-as style="cheerful">
+        <prosody rate="medium">
+          <s>That wraps up our episode.</s>
+          <s>Thanks for listening!<break time="1s"/></s>
+        </prosody>
+      </mstts:express-as>
+    </voice>
+  `;
+
   ssml += `</speak>`;
-  
+
   return { ssml, summary };
 }
 
@@ -190,12 +247,11 @@ async function reddit2podcast(context) {
     const episodeId = new Date().toISOString().split('T')[0];
 
     const threads = await getTopThreads(subreddit);
-    const jsonUrl = await uploadJsonToBlobStorage(threads, `json/episode-${episodeId}.threads.json`);
-
     const { ssml, summary } = await generateSsmlEpisode(threads, context);
-    const ssmlUrl = await uploadXmlToBlobStorage(ssml, `xml/episode-${episodeId}.ssml.xml`);
-
     const audioBuffer = await synthesizeSpeechSsml(ssml);
+
+    const jsonUrl = await uploadJsonToBlobStorage(threads, `json/episode-${episodeId}.threads.json`);
+    const ssmlUrl = await uploadXmlToBlobStorage(ssml, `xml/episode-${episodeId}.ssml.xml`);
     const audioUrl = await uploadAudioToBlobStorage(audioBuffer, `audio/episode-${episodeId}.wav`);
 
     await saveEpisodeMetadata({
