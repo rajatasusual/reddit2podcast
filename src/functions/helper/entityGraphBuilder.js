5b0b109b51cf88f5c665a15d5de4f8f83313f580
Rajatasusual <krajat4@gmail.com>
2025-06-13 14:16:12 +0100

    Updated entity relationships

--- src/functions/helper/entityGraphBuilder.js
+++ src/functions/helper/entityGraphBuilder.js
@@ -85,7 +85,7 @@ class EntityGraphBuilder {
   }
 
   async createRelationshipsBetweenEntities(entities, documentId) {
-     const highConfidenceEntities = entities.filter(e => e.confidenceScore >= 0.7);
+    const highConfidenceEntities = entities.filter(e => e.confidenceScore >= 0.7);
     if (highConfidenceEntities.length > 1) {
       const relationships = await this.createDocumentRelationships(highConfidenceEntities, documentId);
 
@@ -105,11 +105,55 @@ class EntityGraphBuilder {
 
     // Define semantic relationships based on category combinations
     const categoryPairs = {
-      'Person-Organization': 'works_for',
-      'Person-Location': 'located_in',
-      'Organization-Location': 'based_in',
-      'Event-Location': 'occurs_in',
-      'Event-Person': 'involves'
+      "Person-Organization": "works_for",
+      "Person-Location": "resides_in",
+      "Organization-Location": "based_in",
+      "Event-Location": "occurs_in",
+      "Event-Person": "attended_by",
+
+      "Person-Person": "colleague_of",
+      "Person-PersonType": "has_title",
+      "Person-Event": "organized_event",
+      "Person-Product": "uses_product",
+      "Person-Skill": "has_skill",
+      "Person-Address": "lives_at",
+      "Person-PhoneNumber": "has_phone_number",
+      "Person-Email": "has_email",
+      "Person-URL": "has_website",
+      "Person-IP": "last_seen_at_ip",
+      "Person-DateTime": "born_on",
+      "Person-Quantity": "has_age",
+
+      "Organization-Person": "employs",
+      "Organization-Organization": "partner_of",
+      "Organization-Product": "produces",
+      "Organization-Event": "sponsors",
+      "Organization-Address": "headquartered_at",
+      "Organization-PhoneNumber": "has_contact_number",
+      "Organization-Email": "has_contact_email",
+      "Organization-URL": "official_website",
+      "Organization-DateTime": "founded_on",
+      "Organization-Quantity": "has_employee_count",
+      "Organization-IP": "owns_ip_range",
+      "Organization-Skill": "requires_skill",
+
+      "Event-Organization": "hosted_by",
+      "Event-Product": "featured_product",
+      "Event-DateTime": "scheduled_for",
+      "Event-Address": "held_at_address",
+      "Event-URL": "has_event_page",
+      "Event-Quantity": "expected_attendees",
+
+      "Product-Organization": "manufactured_by",
+      "Product-Location": "sold_in",
+      "Product-DateTime": "released_on",
+      "Product-Quantity": "has_price",
+      "Product-URL": "has_product_page",
+      "Product-Skill": "requires_skill_to_operate",
+
+      "Skill-PersonType": "skill_for_role",
+      "Address-Location": "is_in_city_or_country",
+      "PersonType-Organization": "role_within_organization"
     };
 
     const pairKey = `${entity1.category}-${entity2.category}`;
